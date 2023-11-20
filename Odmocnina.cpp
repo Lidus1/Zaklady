ec84ace3c175ce55a159ae08d88ea56986fe7aae
Ludmila Pačandová <128587728+Lidus1@users.noreply.github.com>
2023-11-20 20:00:54 +0000

    if/ else

--- Odmocnina.cpp
+++ Odmocnina.cpp
@@ -9,17 +9,20 @@ using namespace std;
 int main()
 {
     cout << "Zadej nejake cislo, ze ktereho spocitam odmocninu:" << endl;
-    int a;
-    cin >> a;
-    if (a >= 0)
-    {
-        cout << "Zadal jsi nezaporne cislo, to znamena, ze ho mohu odmocnit!" << endl;
-        double o = sqrt(double(a));
-        cout << "Odmocnina z cisla " << a << " je " << o << endl;
-    }
-    cout << "Dekuji za zadani" << endl;
-    cin.get(); cin.get();
-    return 0;
+int a;
+cin >> a;
+if (a >= 0)
+{
+    cout << "Zadal jsi nezaporne cislo, to znamena, ze ho mohu odmocnit!" << endl;
+    double o = sqrt(double(a));
+    cout << "Odmocnina z cisla " << a << " je " << o << endl;
+}
+else
+    cout << "Odmocnina ze zaporneho cisla neexistuje!" << endl;
+cout << "Dekuji za zadani" << endl;
+cin.get(); cin.get();
+
+return 0;
 }
 
 
